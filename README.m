6660261fd7c8665f1a46745537316bb34bfe761e
nicogallardo2704-svg <nicogallardo2704@gmail.com>
2026-07-19 05:23:17 -0300

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,11 @@
+# 🚨 Detector de Spam con Machine Learning
+
+Este repositorio contiene un clasificador de texto inteligente capaz de identificar de forma automática si un mensaje recibido es correo no deseado (Spam) o un mensaje legítimo.
+
+## 🛠️ Tecnologías utilizadas
+* **Python 3**
+* **Scikit-Learn** (Algoritmo Naive Bayes)
+* **CountVectorizer** (Procesamiento de texto)
+
+## 📈 Aplicación en el mundo real
+Este modelo sirve para optimizar sistemas de mensajería, soporte técnico automatizado y filtrado de correos masivos en empresas.
